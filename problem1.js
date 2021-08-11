c9349d7d4e86ef80da71ce6b2a20f98cadb89195
Vasanthakumar B <88326038+vasanth412@users.noreply.github.com>
2021-08-11 08:03:19 +0530

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,1 +1,6 @@
-console.log("hii");
+export default function problem1(inventory){
+    let carID = 33;
+    const car = inventory[carID-1];
+    //const carDetails = 'car 33 is ${car.car_year} ${car.car_make} ${car.car_model}';
+    return carDetails;
+}
